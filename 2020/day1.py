05136ef992976830322e3d79fb4a812e8d496269
kdmontero <kevindmontero@gmail.com>
2020-12-02 23:38:20 +0800

    modified day1 and 2 for a more precise code

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,14 +1,11 @@
-expenses = []
 with open('day1.txt') as given:
-    for line in given.readlines():
-        expenses.append(int(line.rstrip('\n')))
-
+    expenses = [int(expense) for expense in given.read().split('\n')]
 
 # PART 1
 for i, num1 in enumerate(expenses):
-    for j, num2 in enumerate(expenses[i+1:]):
+    for num2 in expenses[i+1:]:
         if num1 + num2  == 2020:    
-            print(num1*num2)
+            print(num1*num2) # 468051
             break
     else:
         continue
@@ -18,9 +15,9 @@ for i, num1 in enumerate(expenses):
 # PART 2
 for i, num1 in enumerate(expenses):
     for j, num2 in enumerate(expenses[i+1:]):
-        for k, num3 in enumerate(expenses[j+1:]):
+        for num3 in expenses[j+1:]:
             if num1 + num2 + num3 == 2020:    
-                print(num1*num2*num3)
+                print(num1*num2*num3) # 272611658
                 break
         else:
             continue
--- 2020/day2.py
+++ 2020/day2.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 def parse(text):
     lmin = text.split('-')[0]
     lmax = text.split('-')[1].split()[0]
@@ -7,20 +5,17 @@ def parse(text):
     pw = text.split('-')[1].split()[2]
     return (lmin, lmax, letter, pw)
 
-passwords = []
 with open('day2.txt') as given:
-    for line in given.readlines():
-        passwords.append(parse(line.rstrip('\n')))
+    passwords = [parse(line) for line in given.read().split('\n')]
 
 valid1 = 0
 valid2 = 0
 for password in passwords:
     lmin, lmax, letter, pw = password
-    c = Counter(pw)
-    if int(lmin) <= c[letter] <= int(lmax):
+    if int(lmin) <= pw.count(letter) <= int(lmax):
         valid1 += 1
-    if (pw[int(lmin)-1] == letter and pw[int(lmax)-1] != letter) or (pw[int(lmin)-1] != letter and pw[int(lmax)-1] == letter):
+    if bool(pw[int(lmin)-1] == letter) != bool(pw[int(lmax)-1] == letter): # XOR: bool(x) != bool(y)
         valid2 += 1
 
-print(f'There are {valid1} valid passwords in part 1')
-print(f'There are {valid2} valid passwords in part 2')
+print(f'There are {valid1} valid passwords in part 1') # 445
+print(f'There are {valid2} valid passwords in part 2') # 491
