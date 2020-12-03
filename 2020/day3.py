b58b7894c5d12d026af5ff3d1aa5397d849cf4c1
kdmontero <kevindmontero@gmail.com>
2020-12-03 16:57:36 +0800

    changed variable names

--- 2020/day3.py
+++ 2020/day3.py
@@ -2,22 +2,22 @@ with open('day3.txt') as given:
     unit_map = given.read().split('\n')
 
 def walk(map_, right, down):
-    init_height = len(map_)
-    init_width = len(map_[0])
+    height = len(map_)
+    width = len(map_[0])
     
     reverse_map = list(zip(*map_))
     full_map = reverse_map[:]
     right, down = down, right
-    init_height, init_width = init_width, init_height
-    height = init_height
+    height, width = width, height
+    cur_height = height
 
     trees = 0
     y = 0
     x = 0
-    while x < init_width:
-        if y >= height:
+    while x < width:
+        if y >= cur_height:
             full_map += reverse_map
-            height += init_height
+            cur_height += height
         if full_map[y][x] == '#':
             trees += 1
         y += down
