d18049d0c20d5f138054931878d9bd4f49ff52a6
kdmontero <kevindmontero@gmail.com>
2020-12-03 19:44:59 +0800

    added day 5 and 6

---
+++ 2019/day5.py
@@ -0,0 +1,122 @@
+with open('day5.txt') as given:
+	program = [int(num) for num in given.read().split(',')]
+
+def opcode(num):
+	return int(str(num)[-2:])
+	
+def mode1(num):
+	if len(str(num))>=3:
+		return int(str(num)[-3])
+	else:
+		return 0
+
+def mode2(num):
+	if len(str(num))>=4:
+		return int(str(num)[-4])
+	else:
+		return 0
+		
+def mode3(num):
+	if len(str(num))>=5:
+		return int(str(num)[-5])
+	else:
+		return 0
+
+def run(program, input_num):
+	i=0
+	while i < len(program)+1:
+		if opcode(program[i]) == 1:
+			if mode1(program[i]) == 1:
+				parameter1 = program[i+1]
+			elif mode1(program[i]) == 0:
+				parameter1 = program[program[i+1]]
+			if mode2(program[i]) == 1:
+				parameter2 = program[i+2]
+			elif mode2(program[i]) == 0:
+				parameter2 = program[program[i+2]]
+			program[program[i+3]] = parameter1 + parameter2
+			i += 4
+		elif opcode(program[i]) == 2:
+			if mode1(program[i]) == 1:
+				parameter1 = program[i+1]
+			elif mode1(program[i]) == 0:
+				parameter1 = program[program[i+1]]
+			if mode2(program[i]) == 1:
+				parameter2 = program[i+2]
+			elif mode2(program[i]) == 0:
+				parameter2 = program[program[i+2]]
+			program[program[i+3]] = parameter1 * parameter2
+			i += 4
+		elif opcode(program[i]) == 3:
+			program[program[i+1]] = input_num
+			i += 2
+		elif opcode(program[i]) == 4:
+			output = program[program[i+1]]
+			# print(output) # shows all the output
+			i += 2
+		elif opcode(program[i]) == 5:
+			if mode1(program[i]) == 1:
+				parameter1 = program[i+1]
+			elif mode1(program[i]) == 0:
+				parameter1 = program[program[i+1]]
+			if mode2(program[i]) == 1:
+				parameter2 = program[i+2]
+			elif mode2(program[i]) == 0:
+				parameter2 = program[program[i+2]]
+			if parameter1 != 0:
+				i = parameter2
+			else:
+				i += 3
+		elif opcode(program[i]) == 6:
+			if mode1(program[i]) == 1:
+				parameter1 = program[i+1]
+			elif mode1(program[i]) == 0:
+				parameter1 = program[program[i+1]]
+			if mode2(program[i]) == 1:
+				parameter2 = program[i+2]
+			elif mode2(program[i]) == 0:
+				parameter2 = program[program[i+2]]
+			if parameter1 == 0:
+				i = parameter2
+			else:
+				i += 3
+		elif opcode(program[i]) == 7:
+			if mode1(program[i]) == 1:
+				parameter1 = program[i+1]
+			elif mode1(program[i]) == 0:
+				parameter1 = program[program[i+1]]
+			if mode2(program[i]) == 1:
+				parameter2 = program[i+2]
+			elif mode2(program[i]) == 0:
+				parameter2 = program[program[i+2]]
+			if parameter1 < parameter2:
+				program[program[i+3]] = 1
+			else:
+				program[program[i+3]] = 0
+			i += 4
+		elif opcode(program[i]) == 8:
+			if mode1(program[i]) == 1:
+				parameter1 = program[i+1]
+			elif mode1(program[i]) == 0:
+				parameter1 = program[program[i+1]]
+			if mode2(program[i]) == 1:
+				parameter2 = program[i+2]
+			elif mode2(program[i]) == 0:
+				parameter2 = program[program[i+2]]
+			if parameter1 == parameter2:
+				program[program[i+3]] = 1
+			else:
+				program[program[i+3]] = 0
+			i += 4
+		elif opcode(program[i]) == 99:
+			return output
+		else:
+			break
+
+# part 1
+program1 = program[:]
+print(run(program1, 1)) # 7692125
+
+# part 2
+program2 = program[:]
+print(run(program2, 5)) # 14340395
---
+++ 2019/day6.py
@@ -0,0 +1,50 @@
+with open('day6.txt') as given:
+	data_map = {}
+	for line in given.read().split('\n'):
+		data_map[line[4:]] = line[:3]
+
+# part 1
+orbital = {}
+exorbit = []
+for planet, orbit in data_map.items():
+	orbital[planet] = [orbit]
+	exorbit.append(orbit)
+	if orbit in orbital.keys():
+		orbital[planet] += orbital[orbit]
+	if planet in exorbit:
+		for x, y in orbital.items():
+			if planet in y:
+				orbital[x] += orbital[planet]
+				
+total = 0
+for orbits in orbital.values():
+	total += len(orbits)
+	
+print(total) # 151345
+
+
+# part 2
+common = []
+for x in orbital["YOU"]:
+	for y in orbital["SAN"]:
+		if x == y:
+			common.append(x)
+			break
+
+def transfer(a,b):
+	steps = 0
+	while a != b:
+		a = data_map[a]
+		steps += 1
+	return steps
+
+min_you = min_san = len(data_map)
+for orbit in common:
+	steps_you = transfer("YOU", orbit)
+	steps_san = transfer("SAN", orbit)
+	if steps_you < min_you:
+		min_you = steps_you
+	if steps_san < min_san:
+		min_san = steps_san
+	
+print(min_you + min_san - 2) # 391
