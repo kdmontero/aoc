f1a7ebd149a63d0be050144e31f83deb3ee312fc
kdmontero <kevindmontero@gmail.com>
2020-12-26 21:03:27 +0800

    added day 4

---
+++ 2015/day04.py
@@ -0,0 +1,26 @@
+import hashlib
+
+key = 'ckczppom'
+
+# part 1
+i = 0
+while True:
+    value = key + str(i)
+    md5_hash = hashlib.md5(value.encode())
+    if md5_hash.hexdigest().startswith('00000'):
+        break
+    i += 1
+
+print(f'Part 1: {i}') # 117946
+
+
+# part 2
+i = 0
+while True:
+    value = key + str(i)
+    md5_hash = hashlib.md5(value.encode())
+    if md5_hash.hexdigest().startswith('000000'):
+        break
+    i += 1
+
+print(f'Part 2: {i}') # 3938038
