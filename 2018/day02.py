0b54f386ff0073ae46b460b9c7ecff16820f8fd5
kdmontero <kevindmontero@gmail.com>
2020-12-27 00:53:59 +0800

    added day 2 2018

---
+++ 2018/day02.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+with open('day02.txt') as f:
+    ids = f.read().splitlines()
+
+# part 1
+ids_2 = ids_3 = 0
+for id_ in ids:
+    c = Counter(id_)
+    if 2 in c.values():
+        ids_2 += 1
+    if 3 in c.values():
+        ids_3 += 1
+
+print(f'Part 1: {ids_2 * ids_3}') # 8296
+
+
+# part 2
+for j, id1 in enumerate(ids):
+    for id2 in ids[j+1:]:
+        found = False
+        common = ''
+        for i, letters in enumerate(zip(id1, id2)):
+            if letters[0] == letters[1]:
+                common += letters[0]
+            elif letters[0] != letters[1] and not found:
+                found = True
+            elif letters[0] != letters[1] and found:
+                break
+        else:
+            break
+    else:
+        continue
+    break
+
+print(f'Part 2: {common}') # pazvmqbftrbeosiecxlghkwud
