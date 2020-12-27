1c41aeeb8827df560391084797519b50b5b4de45
kdmontero <kevindmontero@gmail.com>
2020-12-27 16:32:20 +0800

    added day 25 2015

---
+++ 2015/day25.py
@@ -0,0 +1,17 @@
+given = 'To continue, please consult the code grid in the manual.  Enter the code at row 2978, column 3083.'
+
+# part 1
+ROW = 2_978
+COL = 3_083
+MULT = 252_533
+DIV = 33_554_393
+height = ROW + COL - 2
+i = ((height * (height + 1)) // 2) + COL
+code = 20151125
+for _ in range(i-1):
+    code = (code * MULT) % DIV
+
+print(f'Part 1: {code}') # 2650453
+
+# part 2
+print('Part 2: Complete all 49 stars in Advent of Code 2015')
