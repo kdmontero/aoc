d5a6da91f3cd917c5c0ea5fd0b325e2802b53ca1
kdmontero <kevindmontero@gmail.com>
2021-01-01 02:36:24 +0800

    added day 19 part 1

---
+++ 2019/day19.py
@@ -0,0 +1,185 @@
+with open('day19.txt') as given:
+	intcode = [int(num) for num in given.read().split(',')]
+
+intcode += [0]*10000
+
+class Tractor:
+    def __init__(self):
+        self.relbase = 0
+        self.index = 0
+        self.program = intcode[:]
+    
+    def run(self, y, x):
+        opcode = lambda num: int(str(num)[-2:])
+        mode1 = lambda num: int(str(num)[-3]) if len(str(num))>=3 else 0
+        mode2 = lambda num: int(str(num)[-4]) if len(str(num))>=4 else 0
+        mode3 = lambda num: int(str(num)[-5]) if len(str(num))>=5 else 0
+
+        i = self.index
+        module = self.program
+        input_list = [y, x]
+
+        while i < len(module)+1:
+            if opcode(module[i]) == 1:
+                if mode1(module[i]) == 1:
+                    parameter1 = module[i+1]
+                elif mode1(module[i]) == 0:
+                    parameter1 = module[module[i+1]]
+                elif mode1(module[i]) == 2:
+                    parameter1 = module[module[i+1]+self.relbase]
+                if mode2(module[i]) == 1:
+                    parameter2 = module[i+2]
+                elif mode2(module[i]) == 0:
+                    parameter2 = module[module[i+2]]
+                elif mode2(module[i]) == 2:
+                    parameter2 = module[module[i+2]+self.relbase]
+                if mode3(module[i]) == 1:
+                    parameter3 = i+3
+                    module[parameter3] = parameter1 + parameter2
+                elif mode3(module[i]) == 0:
+                    parameter3 = module[i+3]
+                    module[parameter3] = parameter1 + parameter2
+                elif mode3(module[i]) == 2:
+                    parameter3 = module[i+3]+self.relbase
+                    module[parameter3] = parameter1 + parameter2
+                i += 4
+            elif opcode(module[i]) == 2:
+                if mode1(module[i]) == 1:
+                    parameter1 = module[i+1]
+                elif mode1(module[i]) == 0:
+                    parameter1 = module[module[i+1]]
+                elif mode1(module[i]) == 2:
+                    parameter1 = module[module[i+1]+self.relbase]
+                if mode2(module[i]) == 1:
+                    parameter2 = module[i+2]
+                elif mode2(module[i]) == 0:
+                    parameter2 = module[module[i+2]]
+                elif mode2(module[i]) == 2:
+                    parameter2 = module[module[i+2]+self.relbase]
+                if mode3(module[i]) == 1:
+                    parameter3 = i+3
+                    module[parameter3] = parameter1 * parameter2
+                elif mode3(module[i]) == 0:
+                    parameter3 = module[i+3]
+                    module[parameter3] = parameter1 * parameter2
+                elif mode3(module[i]) == 2:
+                    parameter3 = module[i+3]+self.relbase
+                    module[parameter3] = parameter1 * parameter2
+                i += 4
+            elif opcode(module[i]) == 3:
+                if mode1(module[i]) == 0:
+                    module[module[i+1]] = input_list.pop(0)
+                elif mode1(module[i]) == 2:
+                    module[module[i+1]+self.relbase] = input_list.pop(0)
+                i += 2
+            elif opcode(module[i]) == 4:
+                if mode1(module[i]) == 0:
+                    output = module[module[i+1]]
+                elif mode1(module[i]) == 1:
+                    output = module[i+1]
+                elif mode1(module[i]) == 2:
+                    output = module[module[i+1]+self.relbase]
+                i += 2
+                self.index = i
+                return output
+            elif opcode(module[i]) == 5:
+                if mode1(module[i]) == 1:
+                    parameter1 = module[i+1]
+                elif mode1(module[i]) == 0:
+                    parameter1 = module[module[i+1]]
+                elif mode1(module[i]) == 2:
+                    parameter1 = module[module[i+1]+self.relbase]
+                if mode2(module[i]) == 1:
+                    parameter2 = module[i+2]
+                elif mode2(module[i]) == 0:
+                    parameter2 = module[module[i+2]]
+                elif mode2(module[i]) == 2:
+                    parameter2 = module[module[i+2]+self.relbase]
+                if parameter1 != 0:
+                    i = parameter2
+                else:
+                    i += 3
+            elif opcode(module[i]) == 6:
+                if mode1(module[i]) == 1:
+                    parameter1 = module[i+1]
+                elif mode1(module[i]) == 0:
+                    parameter1 = module[module[i+1]]
+                elif mode1(module[i]) == 2:
+                    parameter1 = module[module[i+1]+self.relbase]
+                if mode2(module[i]) == 1:
+                    parameter2 = module[i+2]
+                elif mode2(module[i]) == 0:
+                    parameter2 = module[module[i+2]]
+                elif mode2(module[i]) == 2:
+                    parameter2 = module[module[i+2]+self.relbase]
+                if parameter1 == 0:
+                    i = parameter2
+                else:
+                    i += 3
+            elif opcode(module[i]) == 7:
+                if mode1(module[i]) == 1:
+                    parameter1 = module[i+1]
+                elif mode1(module[i]) == 0:
+                    parameter1 = module[module[i+1]]
+                elif mode1(module[i]) == 2:
+                    parameter1 = module[module[i+1]+self.relbase]
+                if mode2(module[i]) == 1:
+                    parameter2 = module[i+2]
+                elif mode2(module[i]) == 0:
+                    parameter2 = module[module[i+2]]
+                elif mode2(module[i]) == 2:
+                    parameter2 = module[module[i+2]+self.relbase]
+                if mode3(module[i]) == 0:
+                    parameter3 = module[i+3]
+                elif mode3(module[i]) == 2:
+                    parameter3 = module[i+3]+self.relbase
+                if parameter1 < parameter2:
+                    module[parameter3] = 1
+                else:
+                    module[parameter3] = 0
+                i += 4
+            elif opcode(module[i]) == 8:
+                if mode1(module[i]) == 1:
+                    parameter1 = module[i+1]
+                elif mode1(module[i]) == 0:
+                    parameter1 = module[module[i+1]]
+                elif mode1(module[i]) == 2:
+                    parameter1 = module[module[i+1]+self.relbase]
+                if mode2(module[i]) == 1:
+                    parameter2 = module[i+2]
+                elif mode2(module[i]) == 0:
+                    parameter2 = module[module[i+2]]
+                elif mode2(module[i]) == 2:
+                    parameter2 = module[module[i+2]+self.relbase]
+                if mode3(module[i]) == 0:
+                    parameter3 = module[i+3]
+                elif mode3(module[i]) == 2:
+                    parameter3 = module[i+3]+self.relbase
+                if parameter1 == parameter2:
+                    module[parameter3] = 1
+                else:
+                    module[parameter3] = 0
+                i += 4
+            elif opcode(module[i]) == 9:
+                if mode1(module[i]) == 0:
+                    self.relbase += module[module[i+1]]
+                elif mode1(module[i]) == 1:
+                    self.relbase += module[i+1]
+                elif mode1(module[i]) == 2:
+                    self.relbase += module[module[i+1]+self.relbase]
+                i += 2
+            elif opcode(module[i]) == 99:
+                return
+            else:
+                print("Program is not working")
+                return
+
+
+# part 1
+affected = 0
+for y in range(50):
+    for x in range(50):
+        if Tractor().run(y, x):
+            affected += 1
+
+print(f'Part 1: {affected}') # 183
