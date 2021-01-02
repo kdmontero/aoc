ac2bbed9e2a87b1b45938f75a762a3f79d859e3a
kdmontero <kevindmontero@gmail.com>
2021-01-02 16:06:35 +0800

    added day 8

---
+++ 2015/day08.py
@@ -0,0 +1,33 @@
+with open('day08.txt') as f:
+    strings = []
+    y = 0
+    for string in f.read().splitlines():
+        strings.append(string)
+
+# part 1
+disregard = 0
+for string in strings:
+    i = 1
+    disregard += 2
+    while i < len(string) - 1:
+        if string[i] == '\\':
+            if string[i+1] == 'x':
+                i += 3
+                disregard += 3
+                continue
+            else:
+                disregard += 1
+                i += 2
+                continue
+        i += 1
+
+print(f'Part 1: {disregard}') # 1371
+
+
+# part 2
+additional = 0
+for string in strings:
+    additional += 2
+    additional += string.count('\\') + string.count('"')
+
+print(f'Part 2: {additional}') # 2117
