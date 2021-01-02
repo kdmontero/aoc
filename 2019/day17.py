26fe34ddb9ece4d7b79e532d969eaf3b53dffa17
kdmontero <kevindmontero@gmail.com>
2021-01-03 00:12:06 +0800

    modified string compression algorithm
    modified for more generic solution, yet one edge case will still not work

--- 2019/day17.py
+++ 2019/day17.py
@@ -217,8 +217,9 @@ for y, line in enumerate(bot.scaffold):
     for x, char in enumerate(line):
         if x in {0, len(line)-1}:
             continue
-        if char == bot.scaffold[y-1][x] == bot.scaffold[y+1][x] == bot.scaffold[y][x-1] == bot.scaffold[y][x+1] == '#':
-            total += (x * y)
+        if (char == bot.scaffold[y-1][x] == bot.scaffold[y+1][x] == '#' and
+            bot.scaffold[y][x-1] == bot.scaffold[y][x+1] == '#'):
+                total += (x * y)
 
 print(f'Part 1: {total}') # 5788
 
@@ -317,26 +318,29 @@ def convert_to_input(routine):
     input_list += [10]
     return input_list
 
-def compress_string(string, patterns):
+def compress_string(string, patterns):  # not a good algorithm, one edge case won't work
     if len(patterns) == 3:
         if string == '':
             return patterns
         else:
             return False
     else:
-        for i in range(min(20, len(string))):
-            new_str = string.replace(string[:i+1],'')
-            patterns[string[:i+1]] = len(patterns)
-            check_pattern = compress_string(new_str, patterns)
-            if not check_pattern:
-                del patterns[string[:i+1]]
-                continue
+        remaining = 3 - len(patterns)
+        for i in range(min(21, len(string))-remaining+1,-1,-1):
+            if string[i] == ',' and ('0' <= string[i-1] <= '9'):
+                new_str = string.replace(string[:i+1],'')
+                patterns[string[:i+1]] = len(patterns)
+                check_pattern = compress_string(new_str, patterns)
+                if not check_pattern:
+                    del patterns[string[:i+1]]
+                    continue
+                else:
+                    return check_pattern
             else:
-                return check_pattern
+                continue
 
 bot_map = Map(bot.scaffold)
 bot_map.find_position()        
-
 while not bot_map.is_halted:
     if bot_map.can_move_front():
         bot_map.move_front()
@@ -381,7 +385,7 @@ dust = bot.run(main_routine, pattern_A, pattern_B, pattern_C, show_visualization
 if show_visualization:
     print(f'Part 1: {total}') # 5788
 print(f'Part 2: {dust}') # 648545
-print(f'Main Pattern: {main}')
-print(f'Pattern A: {A}')
-print(f'Pattern B: {B}')
-print(f'Pattern C: {C}')
+print(f'Main Pattern: {main}') # A,B,A,B,C,B,C,A,B,C
+print(f'Pattern A: {A}') # R,4,R,10,R,8,R,4
+print(f'Pattern B: {B}') # R,10,R,6,R,4
+print(f'Pattern C: {C}') # R,4,L,12,R,6,L,12
