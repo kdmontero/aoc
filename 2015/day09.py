9c01bedaebe2f5815630ad1c683f9803fc647896
kdmontero <kevindmontero@gmail.com>
2021-01-03 19:40:38 +0800

    added day 9

---
+++ 2015/day09.py
@@ -0,0 +1,35 @@
+import itertools
+
+with open('day09.txt') as f:
+    paths = {}
+    for line in f.read().splitlines():
+        way, distance = line.split(' = ')
+        place1, place2 = way.split(' to ')
+        if place1 not in paths:
+            paths[place1] = {place2: int(distance)}
+        else:
+            paths[place1][place2] = int(distance)
+        if place2 not in paths:
+            paths[place2] = {place1: int(distance)}
+        else:
+            paths[place2][place1] = int(distance)
+
+def get_total_distance(path: list) -> int:
+    starting = path.pop(0)
+    distance = 0
+    while path:
+        destination = path.pop(0)
+        distance += paths[starting][destination]
+        starting = destination
+    return distance
+
+min_total = 100000
+max_total = 0
+
+for path in itertools.permutations(paths.keys()):
+    total = get_total_distance(list(path))
+    min_total = min(min_total, total)
+    max_total = max(max_total, total)
+
+print(f'Part 1: {min_total}') # 207 part 1
+print(f'Part 2: {max_total}') # 804 part 2
