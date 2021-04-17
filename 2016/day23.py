3b41d0ae6137e5a2dc905486cc8c5f93e757ebbf
kdmontero <kevindmontero@gmail.com>
2021-04-17 18:54:40 +0800

    added day 23 part 2, optimized the assembunny code which is applicable to most (if not all) aoc given

--- 2016/day23.py
+++ 2016/day23.py
@@ -11,9 +11,35 @@ def isinteger(s):
     except ValueError:
         return False
 
+
+def optimize(instruction):
+    instruction = copy.deepcopy(instruction)
+
+    i = 0
+    while i <= len(instruction) - 5:
+        if instruction[i+2][0] == instruction[i+4][0] == 'jnz':
+            if instruction[i+2][2] == '-2' and instruction[i+4][2] == '-5':
+                if all((
+                    instruction[i][0] == 'inc',
+                    instruction[i+1][0] == instruction[i+3][0] == 'dec'
+                )):
+                    arg_1 = instruction[i+1][1]
+                    arg_2 = instruction[i+3][1]
+                    prod = instruction[i][1]
+
+                    instruction[i] = ['mul', arg_1, arg_2, prod]
+                    instruction[i+1] = ['cpy', 0, arg_1]
+                    instruction[i+2] = ['cpy', 0, arg_2]
+                    instruction[i+3] = ['jnz', 0, 0]
+
+                    i += 4
+        i += 1
+
+    return instruction
+
 def reg_a(reg):
     i = 0
-    ins = copy.deepcopy(given_ins)
+    ins = optimize(copy.deepcopy(given_ins))
     while 0 <= i < len(ins):
         if ins[i][0] == 'inc':
             reg[ins[i][1]] += 1
@@ -61,8 +87,12 @@ def reg_a(reg):
                     else:
                         ins[i+index][0] = 'jnz'
 
+        elif ins[i][0] == 'mul':
+            reg[ins[i][3]] = reg[ins[i][1]] * reg[ins[i][2]]
+
         i += 1
+
     return reg['a']
 
 print(f"Part 1: {reg_a({'a': 7, 'b': 0, 'c': 0, 'd': 0})}") # 13685 - part 1
-print(f"Part 2: {reg_a({'a': 12, 'b': 0, 'c': 0, 'd': 0})}") # 
+print(f"Part 2: {reg_a({'a': 12, 'b': 0, 'c': 0, 'd': 0})}") # 479010245 - part 2
