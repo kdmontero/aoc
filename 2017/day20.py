1cba57ecc2738864eef2642d4490e99bf9d0be78
kdmontero <kevindmontero@gmail.com>
2021-10-02 14:02:14 +0800

    added a sort for velocity and position in case there are equal acceleration vector magnitude

--- 2017/day20.py
+++ 2017/day20.py
@@ -13,13 +13,37 @@ if __name__ == '__main__':
 
 
     # part 1
-    closest = None
     shortest_a_vector = 100000000000
+    shortest_v_vector = 100000000000
+    shortest_p_vector = 100000000000
+    a_sort_particles = []
+    v_sort_particles = []
+    p_sort_particles = []
 
     for particle, a in a_dict.items():
         a_vector = (a[0]**2 + a[1]**2 + a[2]**2)**0.5
         if a_vector < shortest_a_vector:
-            closest = particle
+            a_sort_particles = [particle]
             shortest_a_vector = a_vector
+        elif a_vector == shortest_a_vector:
+            a_sort_particles.append(particle)
 
-    print(f'Part 1: {closest}') # 91
+    for particle in a_sort_particles:
+        v = v_dict[particle]
+        v_vector = (v[0]**2 + v[1]**2 + v[2]**2)**0.5
+        if v_vector < shortest_v_vector:
+            v_sort_particles = [particle]
+            shortest_v_vector = v_vector
+        elif v_vector == shortest_v_vector:
+            v_sort_particles.append(particle)
+
+    for particle in v_sort_particles:
+        p = p_dict[particle]
+        p_vector = (p[0]**2 + p[1]**2 + p[2]**2)**0.5
+        if p_vector < shortest_p_vector:
+            p_sort_particles = [particle]
+            shortest_p_vector = p_vector
+        elif p_vector == shortest_p_vector:
+            p_sort_particles.append(particle)
+
+    print(f'Part 1: {p_sort_particles[0]}') # 91
