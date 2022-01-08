9f198343ad2e37cab41a127d7211ccce75348764
kdmontero <kevindmontero@gmail.com>
2022-01-08 21:55:51 +0800

    modified day 14 for faster solution

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,5 +1,4 @@
 import collections
-from functools import lru_cache
 
 if __name__ == '__main__':
     print('Advent of Code 2021 - Day 14')
@@ -10,27 +9,36 @@ if __name__ == '__main__':
 
         for line in raw_rules.splitlines():
             pair, inserted = line.split(' -> ')
-            rules[pair] = pair[0] + inserted + pair[1]
-
-    # part 1
-
-    def expand(polymer):
-        if len(polymer) == 2:
-            return rules.get(polymer, polymer)
- 
-        else:
-            half = (len(polymer) + 1)//2
-            return expand(polymer[:half]) + expand(polymer[half-1:])[1:]
-
-    def polymerize(polymer, steps):
-        if steps == 1:
-            return expand(polymer)
-        else:
-            return polymerize(expand(polymer), steps - 1)
+            rules[pair] = [pair[0] + inserted, inserted + pair[1]]
 
 
-    polymer = polymerize(template, 10)
-    polymer_qty = collections.Counter(polymer)
-    max_p, min_p = max(polymer_qty.values()), min(polymer_qty.values())
+    def expand(polymer, steps):
+        start, end = polymer[0], polymer[-1]
+        pair_count = collections.Counter()
 
-    print(f'Part 1: {max_p - min_p}') # 2621
+        for i in range(len(polymer) -1):
+            pair = polymer[i] + polymer[i+1]
+            pair_count[pair] += 1
+
+        for _ in range(steps):
+            new_count = collections.Counter()
+            for pair, count in pair_count.items():
+                for result_pair in rules[pair]:
+                    new_count[result_pair] += count
+            pair_count = new_count
+
+        char_count = collections.Counter()
+        char_count[start] = 1
+        char_count[end] = 1
+        for pair, count in pair_count.items():
+            char_count[pair[0]] += count
+            char_count[pair[1]] += count
+
+        quantities = sorted(char_count.values())
+
+        return (quantities[-1] - quantities[0]) // 2
+
+
+    print(f'Part 1: {expand(template, 10)}') # 2621 - part 1
+
+    print(f'Part 2: {expand(template, 40)}') # 2843834241366 - part 2
