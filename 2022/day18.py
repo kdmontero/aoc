c055857c46f46ffe5a90e7c9f10a0aa470237c08
kdmontero <kevindmontero@gmail.com>
2022-12-30 22:07:47 +0800

    added day 18 part 1

---
+++ 2022/day18.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    print('Advent of Code 2022 - Day 18')
+
+    with open('day18.txt') as f:
+        cubes = []
+        for line in f.read().splitlines():
+            x, y, z = [int(i) for i in line.split(',')]
+            cubes.append([x, y, z])
+
+    surface_area = 0
+    for cube in cubes:
+        x, y, z = cube
+        for i in [-1, +1]:
+            if [x+1, y, z] not in cubes:
+                surface_area += 1
+            if [x, y+1, z] not in cubes:
+                surface_area += 1
+            if [x, y, z+1] not in cubes:
+                surface_area += 1
+
+    print(f'Part 1: {surface_area}') #
+
+
+    print(f'Part 2: {0}') #
