74613cfb91c98313b336e85f067ba45b2582787b
kdmontero <kevindmontero@gmail.com>
2023-12-09 23:53:29 +0800

    optimized day 6 using quadratic equation

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,3 +1,6 @@
+import math
+
+
 if __name__ == '__main__':
     print('Advent of Code 2023 - Day 06')
 
@@ -13,26 +16,24 @@ if __name__ == '__main__':
             one_time += t
             one_distance += d
 
+    # solve in O(1) using quadratic equation
+    def ways_to_beat(time: int, distance: int) -> int:
+        x1 = math.ceil((-time + (time**2 - (4*distance))**0.5)/-2)
+        x2 = math.floor((-time - (time**2 - (4*distance))**0.5)/-2)
+        return x2 - x1 + 1
+
 
     # part 1
 
     margin_of_error = 1
     for time, distance in records:
-        winning_scenarios = 0
-        for i in range(1, time + 1):
-            if i * (time - i) > distance:
-                winning_scenarios += 1
-        margin_of_error *= winning_scenarios
+        margin_of_error *= ways_to_beat(time, distance)
     
     print(f'Part 1: {margin_of_error}') # 861300
 
 
     # part 2
     
-    winning_scenarios = 0
-    one_time = int(one_time)
-    one_distance = int(one_distance)
-    for i in range(1, one_time + 1):
-        if i * (one_time - i) > one_distance:
-            winning_scenarios += 1
+    winning_scenarios = ways_to_beat(int(one_time), int(one_distance))
+
     print(f'Part 2: {winning_scenarios}') # 28101347
