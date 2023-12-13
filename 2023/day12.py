535fc30f7d23b1f5577339fce5c3158e5b47664e
kdmontero <kevindmontero@gmail.com>
2023-12-13 21:06:53 +0800

    added day 12 part 2

--- 2023/day12.py
+++ 2023/day12.py
@@ -41,10 +41,47 @@ if __name__ == '__main__':
     print(f'Part 1: {total1}') # 7361
 
 
-    # total2 = 0
-    # for report, damage in zip(reports, damages):
-    #     print(report, damage)
-    #     total2 += count_valid('?'.join([report] * 5), damage * 5)
+    # part 2
+
+    @lru_cache
+    def count_all(report: str, damage: tuple[int], damage_start: bool = False) -> int:
+        if damage_start and report.startswith('.'):
+            return 0
+
+        if report.startswith('?'):
+            return count_all(report.replace('?', '.', 1), damage, damage_start) + count_all(report.replace('?', '#', 1), damage, damage_start)
+
+        if not report and not damage:
+            return 1
+        
+        if not report and damage:
+            return 0
+
+        if report.startswith('.'):
+            return count_all(report.lstrip('.'), damage)
+        
+        # default logic: report.startswith('#')
+        if len(damage) == 0:
+            return 0
+
+        if damage[0] == 1:
+            if len(report) > 1:
+                if report[1] == '#':
+                    return 0
+                else:
+                    return count_all(report[2:], damage[1:])
+            else:
+                return count_all(report[1:], damage[1:])
+
+        damage = list(damage)
+        damage[0] -= 1
+        damage = tuple(damage)
+        return count_all(report[1:], damage, True)
+        
+
+    total2 = 0
+    for report, damage in zip(reports, damages):
+        total2 += count_all('?'.join([report] * 5), damage * 5)
 
             
-    # print(f'Part 2: {total2}') #
+    print(f'Part 2: {total2}') # 83317216247365
