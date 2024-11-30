f472a8e4a9d5de9cc5076c0c4ddcf01fd55865b8
kdmontero <kevindmontero@gmail.com>
2024-11-30 18:27:43 +0800

    added download automation feature

--- initiate_aoc_day.py
+++ initiate_aoc_day.py
@@ -1,6 +1,9 @@
-import sys
 import os
-import subprocess
+import sys
+import time
+
+from requests import HTTPError
+from requests import request
 
 
 # ERROR HANDLERS
@@ -33,30 +36,69 @@ if (int(day) < 1) or (int(day) > 25):
     print('Error: Invalid day')
     exit()
 
-if f'day{day}.py' in os.listdir():
-    print(f'Error: Day {day} script is already initiated')
-    exit()
-
-if f'day{day}.txt' in os.listdir():
-    print(f'Error: Day {day} input is already downloaded')
-    exit()
 
 
 # INITIATE PYTHON SCRIPT
 
-code = (
-    "if __name__ == '__main__':\n"
-    f"    print('Advent of Code {year} - Day {day}')\n\n"
-    f"    with open('day{day}.txt') as f:\n"
-    "        pass\n\n\n"
-    "    print(f'Part 1: {0}') #\n\n\n"
-    "    print(f'Part 2: {0}') #"
-)
+def initiate_script():
+    code = (
+        "if __name__ == '__main__':\n"
+        f"    print('Advent of Code {year} - Day {day}')\n\n"
+        f"    with open('day{day}.txt') as f:\n"
+        "        pass\n\n\n"
+        "    print(f'Part 1: {0}') #\n\n\n"
+        "    print(f'Part 2: {0}') #"
+    )
 
-subprocess.call(f"touch day{day}.py", shell=True)
-subprocess.call(f"touch day{day}.txt", shell=True)
-with open(f'day{day}.py', 'w') as f:
-    f.write(code)
+    with open(f'day{day}.py', 'w') as f:
+        f.write(code)
 
 
-print(f'Day {day} initiated successfully')
+if f'day{day}.py' in os.listdir():
+    print(f'Day {day} script is already initiated')
+else:
+    initiate_script()
+    print(f'Day {day} script initiated successfully')
+
+
+
+# DOWNLOAD INPUT
+
+def download_input():
+    env_dir = os.path.dirname(os.path.abspath(__file__))
+    env_path = os.path.join(env_dir, '.env')
+
+    with open(env_path) as f:
+        session_cookie = f.read().strip()
+
+    url = f'https://adventofcode.com/{year}/day/{int(day)}/input'
+    headers = {
+        'Cookie': session_cookie,
+        'User-Agent': 'kevindmontero@gmail.com'
+    }
+
+
+    for _ in range(5):
+        response = request('GET', url, headers=headers)
+
+        try:
+            response.raise_for_status()
+        except HTTPError:
+            raise HTTPError(response.text)
+            time.sleep(1)
+        else:
+            break
+    else:
+        raise SystemExit('timed out after 5 failed attempts')
+
+
+    with open(f'day{day}.txt', 'w') as f:
+        f.write(response.text.strip())
+
+
+if f'day{day}.txt' in os.listdir():
+    print(f'Day {day} input is already downloaded')
+else:
+    download_input()
+    print(f'Day {day} input downloaded successfully')
+
