01be620c22b239a35f69fb7b74d91a9ca6f02006
kdmontero <kevindmontero@gmail.com>
2024-12-21 16:54:16 +0800

    added working day 15 with terminal animation

--- 2024/day15.py
+++ 2024/day15.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     with open('day15.txt') as f:
         map_, ins = f.read().strip().split('\n\n')
         instructions = ins.replace('\n', '')
+        instructions_length = len(instructions)
 
     def print_grid(grid: list[list[str]]) -> None:
         map_ = [''.join(line) for line in grid]
@@ -31,7 +32,7 @@ if __name__ == '__main__':
             if obj == '@':
                 cur_y, cur_x = y, x
 
-    def move1(y: int, x: int, dir: str) -> bool:
+    def is_movable_then_move(y: int, x: int, dir: str) -> bool:
         obj = grid1[y][x]
         if obj == '#':
             return False
@@ -39,38 +40,54 @@ if __name__ == '__main__':
             return True
             
         if dir == '^':
-            if move1(y - 1, x, dir):
+            if is_movable_then_move(y - 1, x, dir):
                 grid1[y][x] = '.'
                 grid1[y - 1][x] = obj
                 return True
         elif dir == 'v':
-            if move1(y + 1, x, dir):
+            if is_movable_then_move(y + 1, x, dir):
                 grid1[y][x] = '.'
                 grid1[y + 1][x] = obj
                 return True
         elif dir == '>':
-            if move1(y, x + 1, dir):
+            if is_movable_then_move(y, x + 1, dir):
                 grid1[y][x] = '.'
                 grid1[y][x + 1] = obj
                 return True
         elif dir == '<':
-            if move1(y, x - 1, dir):
+            if is_movable_then_move(y, x - 1, dir):
                 grid1[y][x] = '.'
                 grid1[y][x - 1] = obj
                 return True
 
         return False
 
-    for instruction in instructions:
-        if move1(cur_y, cur_x, instruction):
-            if instruction == '^':
-                cur_y -= 1
-            elif instruction == 'v':
-                cur_y += 1
-            elif instruction == '<':
-                cur_x -= 1
-            elif instruction == '>':
-                cur_x += 1
+    def run_part_1(
+        instructions: str,
+        cur_y: int,
+        cur_x: int,
+        animate: bool = False
+    ) -> None:
+
+        for i, instruction in enumerate(instructions):
+
+            if animate:
+                os.system('clear')
+                print(f'Steps Remaining: {instructions_length - i - 1}')
+                print_grid(grid1)
+                time.sleep(.001)
+
+            if is_movable_then_move(cur_y, cur_x, instruction):
+                if instruction == '^':
+                    cur_y -= 1
+                elif instruction == 'v':
+                    cur_y += 1
+                elif instruction == '<':
+                    cur_x -= 1
+                elif instruction == '>':
+                    cur_x += 1
+
+    run_part_1(instructions, cur_y, cur_x)
 
     print(f'Part 1: {get_gps(grid1)}') # 1318523
 
@@ -96,7 +113,59 @@ if __name__ == '__main__':
             if obj == '@':
                 cur_y, cur_x = y, x
 
-    def move2(y: int, x: int, dir: str) -> bool:
+    def move(y: int, x: int, dir: str) -> None:
+        obj = grid2[y][x]
+        if obj in '.#':
+            return
+            
+        if dir == '^':
+            if obj == '[':
+                move(y - 1, x, dir)
+                move(y - 1, x + 1, dir)
+                grid2[y - 1][x] = obj
+                grid2[y - 1][x + 1] = ']'
+                grid2[y][x] = '.'
+                grid2[y][x + 1] = '.'
+            elif obj == ']':
+                move(y - 1, x, dir)
+                move(y - 1, x - 1, dir)
+                grid2[y - 1][x] = obj
+                grid2[y - 1][x - 1] = '['
+                grid2[y][x] = '.'
+                grid2[y][x - 1] = '.'
+            elif obj == '@':
+                move(y - 1, x, dir)
+                grid2[y][x] = '.'
+                grid2[y - 1][x] = obj
+        elif dir == 'v':
+            if obj == '[':
+                move(y + 1, x, dir)
+                move(y + 1, x + 1, dir)
+                grid2[y + 1][x] = obj
+                grid2[y + 1][x + 1] = ']'
+                grid2[y][x] = '.'
+                grid2[y][x + 1] = '.'
+            elif obj == ']':
+                move(y + 1, x, dir)
+                move(y + 1, x - 1, dir)
+                grid2[y + 1][x] = obj
+                grid2[y + 1][x - 1] = '['
+                grid2[y][x] = '.'
+                grid2[y][x - 1] = '.'
+            elif obj == '@':
+                move(y + 1, x, dir)
+                grid2[y][x] = '.'
+                grid2[y + 1][x] = obj
+        elif dir == '>':
+            move(y, x + 1, dir)
+            grid2[y][x] = '.'
+            grid2[y][x + 1] = obj
+        elif dir == '<':
+            move(y, x - 1, dir)
+            grid2[y][x] = '.'
+            grid2[y][x - 1] = obj
+
+    def is_movable(y: int, x: int, dir: str) -> bool:
         obj = grid2[y][x]
         if obj == '#':
             return False
@@ -105,71 +174,60 @@ if __name__ == '__main__':
             
         if dir == '^':
             if obj == '[':
-                if move2(y - 1, x, dir) and move2(y - 1, x + 1, dir):
-                    grid2[y - 1][x] = obj
-                    grid2[y - 1][x + 1] = ']'
-                    grid2[y][x] = '.'
-                    grid2[y][x + 1] = '.'
+                if is_movable(y - 1, x, dir) and is_movable(y - 1, x + 1, dir):
                     return True
             elif obj == ']':
-                if move2(y - 1, x, dir) and move2(y - 1, x - 1, dir):
-                    grid2[y - 1][x] = obj
-                    grid2[y - 1][x - 1] = '['
-                    grid2[y][x] = '.'
-                    grid2[y][x - 1] = '.'
+                if is_movable(y - 1, x, dir) and is_movable(y - 1, x - 1, dir):
                     return True
             elif obj == '@':
-                if move2(y - 1, x, dir):
-                    grid2[y][x] = '.'
-                    grid2[y - 1][x] = obj
+                if is_movable(y - 1, x, dir):
                     return True
         elif dir == 'v':
             if obj == '[':
-                if move2(y + 1, x, dir) and move2(y + 1, x + 1, dir):
-                    grid2[y + 1][x] = obj
-                    grid2[y + 1][x + 1] = ']'
-                    grid2[y][x] = '.'
-                    grid2[y][x + 1] = '.'
+                if is_movable(y + 1, x, dir) and is_movable(y + 1, x + 1, dir):
                     return True
             elif obj == ']':
-                if move2(y + 1, x, dir) and move2(y + 1, x - 1, dir):
-                    grid2[y + 1][x] = obj
-                    grid2[y + 1][x - 1] = '['
-                    grid2[y][x] = '.'
-                    grid2[y][x - 1] = '.'
+                if is_movable(y + 1, x, dir) and is_movable(y + 1, x - 1, dir):
                     return True
             elif obj == '@':
-                if move2(y + 1, x, dir):
-                    grid2[y][x] = '.'
-                    grid2[y + 1][x] = obj
+                if is_movable(y + 1, x, dir):
                     return True
         elif dir == '>':
-            if move2(y, x + 1, dir):
-                grid2[y][x] = '.'
-                grid2[y][x + 1] = obj
+            if is_movable(y, x + 1, dir):
                 return True
         elif dir == '<':
-            if move2(y, x - 1, dir):
-                grid2[y][x] = '.'
-                grid2[y][x - 1] = obj
+            if is_movable(y, x - 1, dir):
                 return True
 
         return False
 
-    for instruction in instructions:
-        os.system('clear')
-        print_grid(grid2)
-        time.sleep(.1)
-        
-        if move2(cur_y, cur_x, instruction):
-            if instruction == '^':
-                cur_y -= 1
-            elif instruction == 'v':
-                cur_y += 1
-            elif instruction == '<':
-                cur_x -= 1
-            elif instruction == '>':
-                cur_x += 1
+    def run_part_2(
+        instructions: str,
+        cur_y: int,
+        cur_x: int,
+        animate: bool = False
+    ) -> None:
 
-    print(f'Part 2: {get_gps(grid2)}') #
+        for i, instruction in enumerate(instructions):
+
+            if animate:
+                os.system('clear')
+                print(f'Steps Remaining: {instructions_length - i - 1}')
+                print_grid(grid2)
+                time.sleep(.001)
+            
+            if is_movable(cur_y, cur_x, instruction):
+                move(cur_y, cur_x, instruction)
+                if instruction == '^':
+                    cur_y -= 1
+                elif instruction == 'v':
+                    cur_y += 1
+                elif instruction == '<':
+                    cur_x -= 1
+                elif instruction == '>':
+                    cur_x += 1
+
+    run_part_2(instructions, cur_y, cur_x)
+
+    print(f'Part 2: {get_gps(grid2)}') # 1337648
 
